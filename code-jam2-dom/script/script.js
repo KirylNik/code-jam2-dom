5d8880bf21f5c0c4276bbafcdbdaabd03f733bbb
KirylNik <ultramylo@gmail.com>
2018-04-16 11:13:38 +0300

    added flexibility in the number of messages

--- code-jam2-dom/script/script.js
+++ code-jam2-dom/script/script.js
@@ -1,3 +1,4 @@
+// Сохдаём массив строк для отображения в компоненте.
 let str1 = 'Fact #1: Banging your head against a wall burns 150 calories an hour.';
 let str2 = 'Fact #2: Human saliva has a boiling point three times that of regular water.';
 let str3 = 'Fact #3: If you lift a kangaroo’s tail off the ground it can’t hop.';
@@ -6,17 +7,30 @@ let str5 = 'Fact #5: Billy goats urinate on their own heads to smell more attrac
 
 let arrayNotification = [str1, str2, str3, str4, str5];
 
+// Функция компонента.
 function displayNotification() {
+    // Если сообщений болше одного, то создать слайдер сообщений.
+    if (arrayNotification.length > 1) {
+        // Создать тело слайдера.
+        document.body.querySelector('.notification-slider').innerHTML = '<i class="fas fa-angle-left button-notification button-notification-prev"></i><div class="notification-slider-points"></div><i class="fas fa-angle-right button-notification button-notification-next"></i>';
+        //Наполнить слайдер необходимым количеством точек.
+        for (let i = 0; i < arrayNotification.length; i++) {
+            document.body.querySelector('.notification-slider-points').innerHTML += '<i class="fas fa-circle"></i>';
+        }
+    };
+    // Получить в переменные сам натификатор и поле для записи текста.
     let elemNotification = document.body.querySelector('.notification');
     let notifMessegeArea = document.body.querySelector('.notification-messege');
-
+    // Записать в поле для текста первое сообщение.
     notifMessegeArea.innerHTML = arrayNotification[0];
+    // Установить флаг с номером текущего сообщения, для работы слайдера.
     displayNotification.currentNotif = 0;
-
+    // Отметить первый элемент слайдера.
     document.body.querySelector('.notification-slider-points').querySelector(`:nth-child(${displayNotification.currentNotif + 1})`).style.color = 'blue';
+    // Получить DOM-элементы кнопок для листания слайдера.
     displayNotification.notificationPrev = document.body.querySelector('.button-notification-prev');
     displayNotification.notificationNext = document.body.querySelector('.button-notification-next');
-    
+    // Установить обработчик для кнопки вперёд.
     displayNotification.notificationNext.onclick = function() {
         if (displayNotification.currentNotif == arrayNotification.length - 1) {
         document.body.querySelector('.notification-slider-points').querySelector(`:nth-child(${displayNotification.currentNotif + 1})`).style.color = 'gray';
@@ -28,10 +42,9 @@ function displayNotification() {
         notifMessegeArea.innerHTML = arrayNotification[displayNotification.currentNotif];
         document.body.querySelector('.notification-slider-points').querySelector(`:nth-child(${displayNotification.currentNotif})`).style.color = 'gray';
         document.body.querySelector('.notification-slider-points').querySelector(`:nth-child(${displayNotification.currentNotif + 1})`).style.color = 'blue';    
-    }
-        
+        };
     };
-
+    // Установить обработчик для кнопки назад.
     displayNotification.notificationPrev.onclick = function() {
         if (displayNotification.currentNotif == 0) {
             document.body.querySelector('.notification-slider-points').querySelector(`:nth-child(${arrayNotification.length})`).style.color = 'blue';
@@ -43,17 +56,18 @@ function displayNotification() {
             document.body.querySelector('.notification-slider-points').querySelector(`:nth-child(${displayNotification.currentNotif + 1})`).style.color = 'gray';
             displayNotification.currentNotif--;
             notifMessegeArea.innerHTML = arrayNotification[displayNotification.currentNotif];
-        }
+        };
     };
-
+    // Получить DOM-элемент кнопки закрытия.
     displayNotification.notificationClose = document.body.querySelector('.notification-close')
+    // Установить обработчик для кнопки закрытия.
     displayNotification.notificationClose.onclick = function () {
         if (document.body.querySelector('[name="disableNotification"]').checked) {
             localStorage.setItem('disableNotification','true')
         };
         elemNotification.style.display = 'none';
     };
-
+    // Обратка для листания и закрытия сообщений клавиатурой.
     document.onkeydown = function (event) {
         if (event.keyCode == 37) {
             displayNotification.notificationPrev.onclick();
@@ -65,7 +79,7 @@ function displayNotification() {
             displayNotification.notificationClose.onclick();
         };
     };
-
+    // Отобразить натификацию, если пользователь не установил галочку о запрете показа. 
     if (!localStorage.getItem('disableNotification')) {
         elemNotification.style.display = 'block';
     };
